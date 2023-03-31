9879e49f1408dfe997254d4972d8b15c135a2db9
Sivaranjani-A <ranjani731997@gmail.com>
2023-03-31 10:21:26 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"; // if type : "module" new type import
 import { MongoClient } from "mongodb";
 import moviesRouter from "./routes/movies.route.js";
+import usersRouter from "./routes/users.route.js";
 
 import dotenv from 'dotenv';
 
@@ -22,7 +23,7 @@ console.log("mongo is connected");
 app.use(express.json()); // middleware for all post requests to convert json data from body into JS Object
 app.use(cors());
 
-
+app.use("/user", usersRouter);
 app.use("/movies", moviesRouter);
 
 
